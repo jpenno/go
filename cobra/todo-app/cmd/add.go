1da9e1ee3e50e08ce43f93ccc1515c3acbc3450b
jpenn <jpenno123@hotmail.com>
2024-12-25 10:17:46 +1100

    add add todo

---
+++ cobra/todo-app/cmd/add.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"cli-todoapp/internal/config"
+	fileio "cli-todoapp/internal/fileIO"
+	"cli-todoapp/internal/todo"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add a todo",
+	Long: `add a todo
+	eg:
+	add "task to do"`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("add called")
+
+		task := args[0]
+		var todos []todo.Todo
+		todos = fileio.GetJson(config.CONFIG.Path, todos)
+		newTodo := todo.New(len(todos), task, false)
+		todos = append(todos, newTodo)
+		fileio.SaveJson(config.CONFIG.Path, todos)
+		fmt.Printf("add: %v\n", task)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
