85cabad7607b297abc2753d15e264822b54e1e86
jpenn <jpenno123@hotmail.com>
2025-02-03 19:34:27 +1100

    update hello world

--- learn-go-with-tests/hello-world/hello_test.go
+++ learn-go-with-tests/hello-world/hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("chirs")
-	want := "Hello, chirs"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("chirs")
+		want := "Hello, chirs"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an ampty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
