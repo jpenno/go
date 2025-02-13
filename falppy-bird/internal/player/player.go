7b17e3c0d82abdaea6892c878be7c6e98d65c79b
jpenn <jpenno123@hotmail.com>
2025-02-13 19:32:23 +1100

    add player movment to flappy-bird

--- falppy-bird/internal/player/player.go
+++ falppy-bird/internal/player/player.go
@@ -1,6 +1,44 @@
 package player
 
-import "fmt"
+import (
+	"fmt"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type Player struct {
+	pos      rl.Vector2
+	velocity rl.Vector2
+	size     rl.Vector2
+	garvity  float32
+}
+
+func PlayerInit() Player {
+	var garvity float32 = 300
+	return Player{
+		garvity:  garvity,
+		pos:      rl.Vector2{X: 100, Y: 100},
+		velocity: rl.Vector2{X: 100, Y: garvity},
+		size:     rl.Vector2{X: 100, Y: 100},
+	}
+}
+
+func (p *Player) Update(dt float32) {
+	if p.velocity.Y < p.garvity {
+		p.velocity.Y += 3_500 * dt
+	}
+
+	p.pos.Y += p.velocity.Y * dt
+
+	if rl.IsKeyPressed(rl.KeySpace) {
+		p.velocity.Y = -800
+		p.pos.X += p.velocity.X * dt
+	}
+}
+
+func (p Player) Draw() {
+	rl.DrawRectangleV(p.pos, p.size, rl.Blue)
+}
 
 func PrintPlayer() {
 	fmt.Println("testing")
--- falppy-bird/main.go
+++ falppy-bird/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"flappy-bird/internal/player"
+	"flappy-bird/internal/game"
+	"fmt"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
 
-	rl.InitWindow(1920, 1080, "flappy bird clone")
+	rl.InitWindow(720, 1080, "flappy bird clone")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
+	game := game.Game{}
+	err := game.Init()
+	if err != nil {
+		fmt.Printf("error: %q", err)
+	}
+
 	for !rl.WindowShouldClose() {
+		game.Update()
+
 		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.DarkGray)
-
-		player.PrintPlayer()
-
+		game.Draw()
 		rl.EndDrawing()
 	}
 }
