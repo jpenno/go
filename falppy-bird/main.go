4cb18d5dd9f2ebb18236f79c8f0bac8ad5f745f5
jpenn <jpenno123@hotmail.com>
2025-02-15 11:09:18 +1100

    tidy up main

--- falppy-bird/main.go
+++ falppy-bird/main.go
@@ -2,13 +2,7 @@ package main
 
 import (
 	"flappy-bird/internal/app"
-	// rl "github.com/gen2brain/raylib-go/raylib"
 )
-
-// const (
-// 	SCREEN_WIDTH  = 720
-// 	SCREEN_HEIGHT = 1080
-// )
 
 func main() {
 	app := app.App{}
